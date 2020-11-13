3b2bae5221fb37e97360f6a9545e04bd19927dcd
mishrakeshav <keshav.sm@somaiya.edu>
2020-11-13 15:33:27 +0530

    update users templates

--- src/users/urls.py
+++ src/users/urls.py
@@ -9,7 +9,8 @@ app_name= 'users'
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="users/login.html"),name="login"),
     path('logout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL},name="logout"),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
 
 
--- src/users/views.py
+++ src/users/views.py
@@ -3,19 +3,26 @@ from django.contrib import messages
 from .forms import UserRegistrationForm
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+def register(requests):
+    if requests.method == 'POST':
+        form = UserRegistrationForm(requests.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created Successfully for {username}')
+            messages.success(requests, f'Account Created Successfully for {username}')
             return redirect('users:login')
         else:
-            messages.success(request, f'Error Setting up the account')
+            messages.success(requests, f'Error Setting up the account')
     else:
         form = UserRegistrationForm()
-    return render(request, 'users/register.html', {'form':form})
+    return render(requests, 'users/register.html', {'form':form})
+
+def profile(requests):
+    form = UserRegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(requests, 'users/profile.html', context)
 
 
 
