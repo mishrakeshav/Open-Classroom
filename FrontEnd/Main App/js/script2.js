bf76a81b9cd731e1f978653d180845dad236d212
aneeshKM <65834684+aneeshKM@users.noreply.github.com>
2020-11-26 19:01:08 +0530

    Updated review and to-do

---
+++ FrontEnd/Main App/js/script2.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+console.log("document is ready");
+  
+
+  $( ".card" ).hover(
+  function() {
+    $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+  }, function() {
+    $(this).removeClass('shadow-lg');
+  }
+);
+  
+// document ready  
+});
