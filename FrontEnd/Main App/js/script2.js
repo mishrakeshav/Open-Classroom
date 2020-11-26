8e2181db6734c617cf4e89bab461385bbe5a9330
Keshav Mishra <keshav.sm@somaiya.edu>
2020-11-26 20:29:58 +0530

    update requirements.txt

--- FrontEnd/Main App/js/script2.js
+++ FrontEnd/Main App/js/script2.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
  // executes when HTML-Document is loaded and DOM is ready
-console.log("document is ready");
   
 
-  $( ".card" ).hover(
-  function() {
-    $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
-  }, function() {
-    $(this).removeClass('shadow-lg');
-  }
-);
+    $( ".card" ).hover(
+    function() {
+      $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+    }, function() {
+      $(this).removeClass('shadow-lg');
+    }
+  );
   
 // document ready  
 });
