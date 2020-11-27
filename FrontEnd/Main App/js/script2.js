3e7ef6b07b93195a6c553d1b7350d51286f1b415
aneeshKM <65834684+aneeshKM@users.noreply.github.com>
2020-11-27 19:38:43 +0530

    Grades,View your work,View submission

--- FrontEnd/Main App/js/script2.js
+++ FrontEnd/Main App/js/script2.js
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
-// $(document).ready(function(){
-//   $("#flip").click(function(){
-//     $("#panel").slideToggle("slow");
-//   });
-// });
-//
-// $('#try').collapse({
-//   toggle:true
-// })
-=======
-$(document).ready(function() {
- // executes when HTML-Document is loaded and DOM is ready
-  
-
-    $( ".card" ).hover(
-    function() {
-      $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
-    }, function() {
-      $(this).removeClass('shadow-lg');
-    }
-  );
-  
-// document ready  
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
 });
->>>>>>> 1d9f3db8c3933a2ba0a211e30918259f033ff625
+
+$('#try').collapse({
+  toggle:true
+})
